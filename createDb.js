d038fbaf1906e9447bc3725fa9f34b0136220d8e
nicoffee <fedoseevn@gmail.com>
2017-11-02 20:38:24 +0500

    add logic

---
+++ createDb.js
@@ -0,0 +1,11 @@
+const User = require('./models/user').User
+
+const user = new User({
+  username: 'Tester',
+  password: 'secret'
+})
+
+user
+  .save()
+  .then((...rest) => console.log(rest))
+  .catch(err => console.log('error'))
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto')
+const mongoose = require('./../libs/mongoose')
 
-const mongoose = require('lib/mongoose')
 const Schema = mongoose.Schema
 
 const schema = new Schema({
@@ -8,8 +8,38 @@ const schema = new Schema({
     type: String,
     unique: true,
     required: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  salt: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: String,
+    unique: true,
+    required: true
   }
 })
 
 schema.methods.encryptPassword = password =>
-  crypto.createHmac('sha1', this.salt).update()
+  crypto
+    .createHmac('sha1', this.salt)
+    .update(password)
+    .digest('hex')
+
+schema
+  .virtual('password')
+  .set(password => {
+    schema._plainPassword = password
+    schema.salt = Math.random() + ''
+    schema.hashedPassword = schema.methods.encryptPassword(password)
+  })
+  .get(() => this._plainPassword)
+
+schema.methods.checkPassword = password =>
+  this.encryptPassword(password) === this.hashedPassword
+
+exports.User = mongoose.model('User', schema)
