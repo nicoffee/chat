f4edcd9cadedbdc1e813a9c5498ad99912f6ec03
nicoffee <fedoseevn@gmail.com>
2017-11-03 18:06:29 +0500

    add logic

--- createDb.js
+++ createDb.js
@@ -1,11 +1,44 @@
-const User = require('./models/user').User
+const mongoose = require('./libs/mongoose')
+const async = require('async')
 
-const user = new User({
-  username: 'Tester',
-  password: 'secret'
+mongoose.set('debug', true)
+
+const open = cb => mongoose.connection.on('open', cb)
+
+const dropDatabase = cb => {
+  const db = mongoose.connection.db
+  db.dropDatabase(cb)
+}
+
+const requireModels = cb => {
+  require('./models/user')
+
+  async.each(
+    Object.keys(mongoose.models),
+    (modelName, cb) => mongoose.models[modelName].ensureIndexes(cb),
+    cb
+  )
+}
+
+const createUsers = cb => {
+  const users = [
+    { username: 'Tester', password: '123' },
+    { username: 'Developer', password: '666' },
+    { username: 'Manager', password: '321' }
+  ]
+
+  async.each(
+    users,
+    (userData, cb) => {
+      const user = new mongoose.models.User(userData)
+      user.save(cb)
+    },
+    cb
+  )
+}
+
+async.series([open, dropDatabase, requireModels, createUsers], err => {
+  console.log(err)
+  mongoose.disconnect()
+  process.exit(err ? 255 : 0)
 })
-
-user
-  .save()
-  .then((...rest) => console.log(rest))
-  .catch(err => console.log('error', err))
--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const config = require('./../config')
+
+mongoose.Promise = global.Promise
 mongoose.connect(config.get('mongoose:uri'), config.get('mongoose:options'))
 
 module.exports = mongoose
