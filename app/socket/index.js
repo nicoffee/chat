919a4273b68d90a1656ae3cd0acdff67f6c5055d
nicoffee <fedoseevn@gmail.com>
2017-11-22 01:10:06 +0500

    fix transport

--- app/socket/index.js
+++ app/socket/index.js
@@ -37,7 +37,8 @@ module.exports = server => {
   const io = require('socket.io').listen(server)
 
   io.set('origins', 'localhost:*')
-  io.set('transports', ['polling'])
+  io.set('transports', ['websocket', 'polling'])
+  io.set('polling duration', 10)
 
   io.use(function(socket, next) {
     async.waterfall(
